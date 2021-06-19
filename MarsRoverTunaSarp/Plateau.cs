4fe6653cf97bca09a6b4889f45e834209ac0ee4b
tsarp96 <tsarp96@hotmail.com>
2021-06-19 20:55:03 +0300

    Add Plateau

---
+++ MarsRoverTunaSarp/Plateau.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRoverTunaSarp
+{
+    public class Plateau
+    {
+        private int horizontalUpperRightBoundry;
+        private int verticalUpperRightBoundry;
+        private int horizontalLoweLeftBoundry;
+        private int verticalLowerLeftBoundry;
+
+        
+        public Plateau(int horizontalUpperRightBoundry, int verticalUpperRightBoundry, int horizontalLoweLeftBoundry = 0, int verticalLowerLeftBoundry = 0) 
+        {
+            this.horizontalLoweLeftBoundry = horizontalLoweLeftBoundry;
+            this.verticalLowerLeftBoundry = verticalLowerLeftBoundry;
+            this.horizontalUpperRightBoundry = horizontalUpperRightBoundry;
+            this.verticalUpperRightBoundry = verticalUpperRightBoundry;
+        }
+
+
+        public int HorizontalUpperRightBoundry { get => horizontalUpperRightBoundry; set => horizontalUpperRightBoundry = value; }
+        public int VerticalUpperRightBoundry { get => verticalUpperRightBoundry; set => verticalUpperRightBoundry = value; }
+        public int HorizontalLoweLeftBoundry { get => horizontalLoweLeftBoundry; set => horizontalLoweLeftBoundry = value; }
+        public int VerticalLowerLeftBoundry { get => verticalLowerLeftBoundry; set => verticalLowerLeftBoundry = value; }
+    }
+}
