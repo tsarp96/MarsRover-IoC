ef5a56a47926010c7fee163dd6cbed09421b3951
tsarp96 <tsarp96@hotmail.com>
2021-06-20 16:03:16 +0300

    Add ExplorationTests with expected Coverage

--- MarsRoverTest_NUnit/ExplorationServiceTest.cs
+++ MarsRoverTest_NUnit/ExplorationServiceTest.cs
@@ -3,12 +3,19 @@ using MarsRoverTunaSarp.Services;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace MarsRoverTest_NUnit
 {
     public class ExplorationServiceTest
     {
+        static List<List<Object>> explorationServiceParametersWithNulls = new List<List<Object>>()
+        {
+            new List<Object>() { new Rover(new Position(1, 2, (int)Compass.N)), new Plateau(5, 5), null },
+            new List<Object>() { null, new Plateau(5, 5), "LMRL" },
+            new List<Object>() { new Rover(new Position(1, 2, (int)Compass.N)), null, "LMR" },
+        };
+
+      
         [Test]
         public void TraceRoute_BoundryBreachDetected_ShouldThrowAnException()
         {
@@ -21,5 +28,30 @@ namespace MarsRoverTest_NUnit
             Assert.Throws<Exception>(() => sut.TraceRoute());
 
         }
+
+        [Test]
+        [TestCaseSource("explorationServiceParametersWithNulls")]
+        public void CreateExplorationService_SendNullParameters_ShouldThrowAnException(List<Object> parameters)
+        {
+            Assert.Throws<ArgumentNullException>(() => new ExplorationService((Rover)parameters[0], (Plateau)parameters[1], (string)parameters[2]));
+        }
+
+        [Test]
+        public void TraceRoute_ParameterIsNormal_RoverShouldBeReallocatedCorrectly()
+        {
+            //Arrange
+            Plateau plateau = new Plateau(5, 5);
+            Rover rover = new Rover(new Position(1, 2, (int)Compass.N));
+            string route = "LMLMLMLMMR";
+            ExplorationService sut = new ExplorationService(rover, plateau, route);
+            //Act
+            sut.TraceRoute();
+            //Assert
+            Assert.That(rover.Position.X, Is.EqualTo(1));
+            Assert.That(rover.Position.Y, Is.EqualTo(3));
+            Assert.That(rover.Position.Direction, Is.EqualTo((int)Compass.E));
+
+        }
+
     }
 }
